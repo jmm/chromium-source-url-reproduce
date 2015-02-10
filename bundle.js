746e6b0f3ae4226a858bacc2a75a6aa14cb858d4
Jesse McCarthy <git_commits@jessemccarthy.net>
2015-02-10 16:33:44 -0500

    Initial commit.

---
+++ bundle.js
@@ -0,0 +1,4 @@
+console.log('x');
+
+//# sourceMappingURL=bundle.map.js
+//# sourceURL=y/a.js
---
+++ bundle.map.js
@@ -0,0 +1,1 @@
+{"version":3,"sources":["x/a.js"],"names":[],"mappings":"AAAA,MAAM,IAAI,AAAC,CAAC,GAAE,CAAC,CAAC;AAAA","file":"x/a.js","sourcesContent":["console.log('x');"]}
